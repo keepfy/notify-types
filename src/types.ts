7cd91b615465408013597e1a7c991d0ebaf20261
Vinícius de F. Marques <vini.type@gmail.com>
2020-02-21 16:48:09 +0000

    refactor: add service request finalization event code

--- src/types.ts
+++ src/types.ts
@@ -7,6 +7,7 @@ export enum EventCode {
     serviceRequestOpening = 'SERVICE_REQUEST_OPENING',
     serviceRequestDistribution = 'SERVICE_REQUEST_DISTRIBUTION',
     serviceRequestCancelation = 'SERVICE_REQUEST_CANCELATION',
+    serviceRequestFinalization = 'SERVICE_REQUEST_FINALIZATION',
     serviceOrderOpening = 'SERVICE_ORDER_OPENING',
     serviceOrderClosening = 'SERVICE_ORDER_CLOSENING',
     serviceOrderAssignment = 'SERVICE_ORDER_ASSIGNMENT',
